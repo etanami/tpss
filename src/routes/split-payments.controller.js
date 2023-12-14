02437f0856aff4440b89234cbf64d4ee0c0fa7f7
Etanami Olatunji <etanamiolatunji@gmail.com>
2023-12-14 10:07:19 +0100

    add: split calculation for flat and percentage type

--- src/routes/split-payments.controller.js
+++ src/routes/split-payments.controller.js
@@ -3,14 +3,27 @@ function splitPayment(req, res) {
 
   let balance = Amount;
   console.log(`Initial balance is: ${balance}`);
-  
 
   const orderedSplitInfo = splitInfoOrder(SplitInfo);
-  //console.log(orderedSplitInfo);
 
+  const SplitBreakdown = orderedSplitInfo.map(splitInfo => {
+    let split = calculateSplit(balance, splitInfo)
+    balance -= split;
+
+    return {
+      SplitEntityId: splitInfo.SplitEntityId,
+      Amount: split
+    }
+  })
+
+  return res.json({
+    ID,
+    Balance: balance,
+    SplitBreakdown
+  })
 }
 
-// sort the entity types pf the request in the specified order
+// sort the entity types of the request in the specified order
 function splitInfoOrder(splitInfo) {
   let order = ["FLAT", "PERCENTAGE", "RATIO"];
 
@@ -20,6 +33,30 @@ function splitInfoOrder(splitInfo) {
 }
 
 
-function calculateSplit() {}
+function calculateSplit(balance, info) {
+  let id = info.SplitEntityId;
+  let value = info.SplitValue;
+  let type = info.SplitType;
+
+  // return split value if type is FLAT
+  if(type === "FLAT") {
+    console.log(`Split amount for "${id}": ${value}`);
+    console.log(`Balance after split calculation for "${id}": (${balance} - ${value})`);
+    console.log(balance - info.SplitValue);
+    return info.SplitValue;
+  }
+  // return split value if type is PERCENTAGE
+  if(type === "PERCENTAGE") {
+    let SplitValue = (value / 100) * balance;
+    console.log(`Split amount for "${id}": (${value} % OF ${balance}) = ${SplitValue}`);
+    console.log(`Balance after split calculation for "${id}": (${balance} - (${SplitValue}))`);
+    console.log(balance - SplitValue);
+    return SplitValue;
+  }
+  // return split value if type is RATIO
+  // if(splitType === "RATIO") {
+  //   console.log("pending");
+  // }
+}
 
 export { splitPayment };
